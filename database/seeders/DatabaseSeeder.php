93244a9222fc4b4e6a49e13d89ec05d4a417aefa
Rudolf HiOk <rodolfo.okino@gmail.com>
2024-10-09 21:03:31 -0300

    create proposal model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Project;
+use App\Models\Proposal;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -19,6 +20,7 @@ class DatabaseSeeder extends Seeder
         User::query()->inRandomOrder()->limit(10)->get()
             ->each(function (User $u) {
                 $project = Project::factory()->create(['created_by' => $u->id]);
+                Proposal::factory()->count(random_int(4, 45))->create(['project_id' => $project->id]);
             });
     }
 }
