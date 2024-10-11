a485bce8de6114fd7886e094461dd72ab4b26ee5
Rudolf HiOk <rodolfo.okino@gmail.com>
2024-10-10 22:48:52 -0300

    implements up and down status proposals

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Actions\ArrangePositions;
 use App\Models\User;
 use App\Models\Project;
 use App\Models\Proposal;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,7 +21,10 @@ class DatabaseSeeder extends Seeder
         User::query()->inRandomOrder()->limit(10)->get()
             ->each(function (User $u) {
                 $project = Project::factory()->create(['created_by' => $u->id]);
+
                 Proposal::factory()->count(random_int(4, 45))->create(['project_id' => $project->id]);
+
+                ArrangePositions::run($project->id);
             });
     }
 }
